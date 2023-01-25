380e7c7d62881b114e57c9bfc669309a736dba71
Daniel Oyola <danieloyola@softerdo.com>
2023-01-25 11:20:45 -0400

    Finished speech.controller file

---
+++ src/speech/speech.controller.js
@@ -0,0 +1,41 @@
+const Speech = require('../models/speech.model')
+
+const getAllSpeech = async () => {
+  const data = await Speech.findAll()
+  return data
+}
+
+const getSpeechById = async (id) => {
+  const data = await Speech.findOne({where: {id}})
+  return data
+}
+
+const createSpeech = async (data) => {
+  const response = await Speech.create({
+    id: data.id,
+    title: data.title,
+    topicId: data.topicId,
+    speakerId: data.speakerId
+  })
+  return response
+}
+
+const updateSpeech = async (id, data) => {
+  const response = await Speech.update({
+    data, where: {id}
+  })
+  return response
+}
+
+const deleteSpeech = async (id) => {
+  const data = await Speech.destroy({where:{id}})
+  return data
+}
+
+module.exports = {
+  getAllSpeech,
+  getSpeechById,
+  createSpeech,
+  updateSpeech,
+  deleteSpeech
+}
